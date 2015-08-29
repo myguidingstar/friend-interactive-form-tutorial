cff9a37d5d1d2b27afbadddfb402f3d0638e84c5
Hoàng Minh Thắng <p@banphim.net>
2015-08-29 13:41:29 +0700

    update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject friend-form-login "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]
-                 [com.cemerick/friend "0.1.2"]
-                 [ring "1.1.3"]]
-  :plugins [[lein-ring "0.7.5"]]
-  :ring {:handler friend-form-login.handler/app}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [compojure "1.4.0"]
+                 [com.cemerick/friend "0.2.1"]]
+  :plugins [[lein-ring "0.9.6"]]
+  :ring {:handler friend-form-login.handler/reloadable-server}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
